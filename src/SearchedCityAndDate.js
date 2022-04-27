98c2b921ce12d2383dad8a02e1a4282c958a329c
JDeB14 <101413157+JDeB14@users.noreply.github.com>
2022-04-27 16:16:39 -0500

    Updated Date and changed btn css

--- src/SearchedCityAndDate.js
+++ src/SearchedCityAndDate.js
@@ -1,16 +1,32 @@
 import React from "react";
 
 export default function SearchedCityAndDate() {
+  let currentDate = new Date();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let day = days[currentDate.getDay()];
+  let month = months[currentDate.getMonth()];
+  let date = currentDate.getDate();
   let weatherData = {
     searchedCity: "Memphis",
-    day: "Sun",
-    date: "April 18",
   };
   return (
     <div>
       <h3 className="default-color">{weatherData.searchedCity}</h3>
       <h6 className="color">
-        {weatherData.day}, {weatherData.date}
+        {day}, {month} {date}
       </h6>
     </div>
   );
